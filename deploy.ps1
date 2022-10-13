abae999b54bbca983ca98d2718104eb250830450
Aurelio Buonomo <auribuo@gmail.com>
2022-10-13 17:37:38 +0200

    Removed actions and fix deploy

--- deploy.ps1
+++ deploy.ps1
@@ -1,26 +1,6 @@
 # build
 npm run build
+git add -f dist/
+git commit -m "deploy"
 
-# navigate into the build output directory
-cd dist
-
-# place .nojekyll to bypass Jekyll processing
-New-Item .nojekyll
-
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
-
-git init
-git remote add origin https://www.github.com/buonotti/bus-stats.git
-git checkout -B gh-pages
-git add -A
-git commit -m 'deploy'
-git push --set-upstream origin gh-pages
-
-# if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
-
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
-
-cd ..
+git subtree push --prefix dist origin gh-pages

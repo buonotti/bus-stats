94ce2d3f16443d5eaccf2771d4a78724ad7ec89e
Aurelio Buonomo <auribuo@gmail.com>
2022-10-13 17:26:12 +0200

    Deploy script

---
+++ deploy.ps1
@@ -0,0 +1,26 @@
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+# place .nojekyll to bypass Jekyll processing
+New-Item .nojekyll
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git remote add origin https://www.github.com/buonotti/bus-stats.git
+git checkout -B gh-pages
+git add -A
+git commit -m 'deploy'
+git push -f origin
+
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+# git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
+
+cd -
